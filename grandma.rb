977dcef6703b381c3bbe36500b05266b0e39dfff
Alexander Bispo <alexbispo173@gmail.com>
2019-09-17 02:14:28 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma(Words)
+def speak_to_grandma(words)
   if words = words.upcase
     if words = "I LOVE YOU GRANDMA!"
-      return I LOVE YOU TOO PUMPKIN!
+      return "I LOVE YOU TOO PUMPKIN!"
     else
       return "NO, NOT SINCE 1938!"
     end  
